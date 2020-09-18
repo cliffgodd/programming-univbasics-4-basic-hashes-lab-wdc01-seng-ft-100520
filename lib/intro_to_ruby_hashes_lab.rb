7ec979f7d57203035634f7208e7c80b2dc4b6af8
Justin Clifford <justoon7.jc@gmail.com>
2020-09-18 22:14:31 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,11 +8,13 @@ def my_hash
   my_hash = {
     name: "Justin", home: "Maryland"
   }
-  my_hash[name:]
   # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
+  pioneer = {
+    :name => 'Grace Hopper'
+  }
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
